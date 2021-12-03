3a1c55be1a50a72712c7c26cd6cf4952094f050d
Adrian Anderson <74568128+andersonadrian@users.noreply.github.com>
2021-12-03 13:01:25 -0800

    Updated the app to flix part 2
    Added the new gif and completed the part two of the app

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Adrian Anderson on 11/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
